6317d5966cb206376de95862dbdc211163100d62
Karol Harasim <harasimkar@gmail.com>
2019-04-26 14:20:10 +0200

    first commit

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from catalog.models import Book, BookInstance, Author, Genre
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Genre)
